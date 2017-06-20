201cd6de7e44b4bd8d2f3f5efc37049493940aa3
vincentwilmet <vincent02pd2016@sasstudent.org>
2017-06-20 17:32:12 -0400

    Scripts to activate django environment

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\vincent\desktop\112\hack112\hack112_virtualenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
